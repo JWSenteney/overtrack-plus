0cc0e622ec970155b243a6251556d71d6575d852
Jacob Senteney <jwsenteney@gmail.com>
2019-11-21 07:12:04 -0500

    Add root level yarn install to initialize script

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,4 @@
+yarn install;
 git clone git@github.com:JWSenteney/overtrack-plus-desktop.git desktop;
 yarn --cwd desktop install;
 git clone git@github.com:JWSenteney/overtrack-plus-server.git server;

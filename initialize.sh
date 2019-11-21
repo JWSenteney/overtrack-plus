8b56ca778f3b299af9a8918420696580a1574d60
Jacob Senteney <jwsenteney@gmail.com>
2019-11-21 06:27:28 -0500

    Remove git submodules in favor of an initialize script

---
+++ initialize.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:JWSenteney/overtrack-plus-desktop.git desktop;
+yarn --cwd desktop install;
+git clone git@github.com:JWSenteney/overtrack-plus-server.git server;
+yarn --cwd server install;
+git clone git@github.com:JWSenteney/overtrack-plus-web.git web;
+yarn --cwd web install;

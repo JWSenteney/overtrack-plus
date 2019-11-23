a4f8ec59fd39940f7ab8932a0719f673090210a9
Jacob Senteney <jwsenteney@gmail.com>
2019-11-22 20:07:09 -0500

    Install router and setup default route

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+const Landing = () => <div>OverTrack+</div>;
 
 const App = () => {
-  return <div className={"App"}>OverTrack+</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;

208a5fd597dcc66ee21c0e3524db04cc4fee6ce9
Jacob Senteney <jwsenteney@gmail.com>
2019-11-22 20:09:09 -0500

    Trim down and refactor logo.svg and display it on landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-const Landing = () => <div>OverTrack+</div>;
+import logo from "../assets/logo.svg";
+
+const Landing = () => (
+  <div>
+    OverTrack+
+    <svg height="50" width="50">
+      <use xlinkHref={`${logo}#logo`} />
+    </svg>
+  </div>
+);
 
 const App = () => {
   return (

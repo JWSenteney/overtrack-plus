11a61f0e5b669b47509f889f646c7bd9a5274d8b
Jacob Senteney <jwsenteney@gmail.com>
2020-02-13 17:19:53 -0500

    Remove web module

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,3 @@
 yarn install;
 git clone git@github.com:JWSenteney/overtrack-plus-desktop.git desktop;
 yarn --cwd desktop install;
-git clone git@github.com:JWSenteney/overtrack-plus-web.git web;
-yarn --cwd web install;

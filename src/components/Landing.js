e929c8c08dbb90f26b4afbe9021b64dc2247023a
Jacob Senteney <jwsenteney@gmail.com>
2020-02-13 00:56:22 -0500

    Remove express api server from project

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,35 +2,25 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Container, Typography } from "@material-ui/core";
 import { Translate } from "react-localize-redux";
-import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
-
-import { testApi } from "../store/test";
 
 class Landing extends Component {
   render = () => {
-    const { classes, test } = this.props;
+    const { classes } = this.props;
+
     return (
       <Container maxWidth="md" className={classes.root}>
         <Typography variant="body1" className={classes.apiCheck}>
-          <Translate id="api_works" />: <Translate id={test ? "yes" : "no"} />
+          <Translate id="navigation.dashboard" />
         </Typography>
       </Container>
     );
   };
-
-  componentDidMount = () => {
-    this.props.testApi();
-  };
 }
 
 Landing.propTypes = {
-  classes: PropTypes.object.isRequired,
-  test: PropTypes.bool.isRequired,
-  testApi: PropTypes.func.isRequired
+  classes: PropTypes.object.isRequired
 };
-
-Landing = connect(({ test }) => ({ test }), { testApi })(Landing);
 
 Landing = withStyles(
   theme => ({

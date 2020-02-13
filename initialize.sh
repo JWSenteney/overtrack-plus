f902df94e83de2ff7438df5db920f44eb731d35c
Jacob Senteney <jwsenteney@gmail.com>
2020-02-13 00:56:25 -0500

    Remove express api server from project

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,5 @@
 yarn install;
 git clone git@github.com:JWSenteney/overtrack-plus-desktop.git desktop;
 yarn --cwd desktop install;
-git clone git@github.com:JWSenteney/overtrack-plus-server.git server;
-yarn --cwd server install;
 git clone git@github.com:JWSenteney/overtrack-plus-web.git web;
 yarn --cwd web install;
